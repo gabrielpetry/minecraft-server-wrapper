22b38d306744a87d537af312367e34b22b47febd
Gabriel petry <contato@gabrielpetry.com.br>
2020-07-17 17:59:09 -0300

    shell colors class

---
+++ shell_colors.py
@@ -0,0 +1,12 @@
+class shell_colors(object):
+    """
+        Short hand for shell colors
+    """
+    magenta = "\033[95m"
+    cyan = "\033[94m"
+    green = "\033[92m"
+    warn = "\033[93m"
+    err = "\033[91m"
+    end = "\033[0m"
+    bold = "\033[1m"
+    undeline = "\033[4m"
